310c57c39ff4d2cf89240339094c12256a9c54dd
bayernfan <jubc86084@hotmail.com>
2017-12-04 23:22:27 +0800

    implementing the Conv2DLayer class.

--- src/gtlearning/neuralnetwork.py
+++ src/gtlearning/neuralnetwork.py
@@ -18,12 +18,13 @@ class SequentialNetwork(object):
         # todo: use the batch_size
 
         for i_epochs in range(epochs):
-            random.shuffle(x_train)
-            for i, inputs in enumerate(x_train):
-                self.layers[0].feed_forward(inputs)
-                self.layers[-1].backpropagate(y_train[i])
             if verbose > 0:
-                print('Epoche {} finished.'.format(i_epochs))
+                print('Epoche ({}) {}'.format(i_epochs, '{'))
+            for inputs, targets in zip(x_train, y_train):
+                outputs = self.layers[0].feed_forward(inputs)
+                self.layers[-1].back_propagate(outputs - targets)
+            if verbose > 0:
+                print('}')
         return self
 
     def predict(self, x, batch_size=None):
@@ -32,7 +33,10 @@ class SequentialNetwork(object):
 
 class Layer(object):
     def __init__(self):
-        self.weights = []
+        self.inputs = None
+        self.outputs = None
+        self.weights = None
+        self.biases = None
         self.shape = None
         self.prev_layer = None
         self.next_layer = None
@@ -48,55 +52,60 @@ class Layer(object):
     def feed_forward(self, inputs):
         pass
 
-    def backpropagate(self, targets):
+    def back_propagate(self, targets):
         pass
 
 
 class DenseLayer(Layer):
     def __init__(self, units, input_shape=None, activation="sigmoid"):
         super(DenseLayer, self).__init__()
+
+        activation = activation.lower()
         if activation == "sigmoid":
             self.activation = SigmoidActivation()
         else:
-            self.activation = activation
+            self.activation = activation  # todo: allow other activations
+
         self.shape = (units,)
+
         self.input_shape = input_shape
         if input_shape is not None:
-            self._fill_weights_and_biases()
+            self._init_weights_and_biases()
 
     def link_to(self, prev_layer):
         if self.input_shape is None:
             self.input_shape = prev_layer.shape
-            self._fill_weights_and_biases()
+            self._init_weights_and_biases()
         return super(DenseLayer, self).link_to(prev_layer)
 
-    def _fill_weights_and_biases(self):
-        self.weights = [[random.random() for j in range(self.input_shape[0])]
-                        for i in range(self.shape[0])]
-        self.biases = [0 for i in range(self.shape[0])]
+    def _init_weights_and_biases(self):
+        self.weights = np.random.rand(self.shape[0], self.input_shape[0])
+        self.biases = np.zeros(self.shape[0])
 
     def feed_forward(self, inputs):
         self.inputs = inputs
-        outputs = self.outputs = [self.activation.activate(np.dot(weights_per_neuron, inputs) + self.biases[i])
-                                  for i, weights_per_neuron in enumerate(self.weights)]
+        outputs = [self.activation.activate(np.dot(weights, inputs) + bias)
+                   for weights, bias in zip(self.weights, self.biases)]
+        self.outputs = outputs = np.array(outputs)
         return self.next_layer.feed_forward(outputs) if self.next_layer else outputs
 
-    def backpropagate(self, targets):
-        output_deltas = [self.activation.derivate(output, output - target)
-                         for output, target in zip(self.outputs, targets)]
-        for i, (weights_per_neuron, output_delta) in enumerate(zip(self.weights, output_deltas)):
-            for j, prev_output in enumerate(self.inputs):
-                self.biases[i] -= output_delta
-                weights_per_neuron[j] -= output_delta * prev_output
-        propagated_targets = [prev_output - np.dot(output_deltas, [n[i] for n in self.weights])
-                              for i, prev_output in enumerate(self.inputs)]
-        return self.prev_layer.backpropagate(propagated_targets) if self.prev_layer else propagated_targets
+    def back_propagate(self, output_deltas):
+        input_deltas = self.activation.derivate(self.outputs, output_deltas)
+        n, m = self.weights.shape;
+        for i in range(n):
+            input_delta = input_deltas[i]
+            self.weights[i] -= input_delta * self.inputs
+            self.biases[i] -= input_delta
+
+        propagated_deltas = [np.dot(input_deltas, self.weights[:, i])
+                             for i in range(m)]
+        propagated_deltas = np.array(propagated_deltas)
+        return self.prev_layer.back_propagate(propagated_deltas) if self.prev_layer else propagated_deltas
 
 
 class FlattenLayer(Layer):
     def __init__(self):
         super(FlattenLayer, self).__init__()
-        self.shape = None
         self.input_shape = None
 
     def link_to(self, prev_layer):
@@ -110,9 +119,94 @@ class FlattenLayer(Layer):
         outputs = self.outputs = np.reshape(inputs, self.shape)
         return self.next_layer.feed_forward(outputs) if self.next_layer else outputs
 
-    def backpropagate(self, targets):
+    def back_propagate(self, targets):
         propagated_targets = np.reshape(targets, self.input_shape)
-        return self.prev_layer.backpropagate(propagated_targets) if self.prev_layer else propagated_targets
+        return self.prev_layer.back_propagate(propagated_targets) if self.prev_layer else propagated_targets
+
+
+class Conv2DLayer(Layer):
+    def __init__(self, filters, kernel_size, strides=(1, 1), padding='valid', input_shape=None, activation="sigmoid"):
+        super(Conv2DLayer, self).__init__()
+
+        self.filters = filters
+        self.kernel_size = kernel_size
+        self.strides = strides
+        self.padding = padding.lower()
+        self.input_shape = input_shape[:3] if len(input_shape) >= 3 else (*input_shape[:2], 1)
+        if input_shape is not None:
+            self._init_shape()
+            self._init_weights_and_biases()
+
+        activation = activation.lower()
+        if activation == "sigmoid":
+            self.activation = SigmoidActivation()
+        else:
+            self.activation = activation  # todo: allow other activations
+
+    def link_to(self, prev_layer):
+        if self.input_shape is None:
+            self.input_shape = prev_layer.shape
+            self._init_shape()
+            self._init_weights_and_biases()
+        return super(Conv2DLayer, self).link_to(prev_layer)
+
+    def _init_shape(self):
+        shape_xy = [self._round_to_int((self.input_shape[i] - (self.kernel_size[i] - 1)) / self.strides[i])
+                    for i in range(2)]
+        self.shape = (*shape_xy, self.filters)
+
+    def _round_to_int(self, x):
+        round_fun = math.floor if self.padding == 'valid' else math.ceil
+        return int(round_fun(x))
+
+    def _init_weights_and_biases(self):
+        shape = self.shape
+        self.weights = np.random.rand(*shape, *self.kernel_size, self.input_shape[2])
+        self.biases = np.zeros(reduce(lambda l, r: l * r, shape)).reshape(shape)
+
+    def feed_forward(self, inputs):
+        if inputs.shape != self.input_shape:
+            inputs = inputs.reshape(self.input_shape)
+        if self.padding == 'same':
+            pass  # todo: allow the 'same' padding
+        self.inputs = inputs
+        stride_x, stride_y = self.strides
+        kernel_x, kernel_y = self.kernel_size
+        n_x, n_y, n_f, m_x, m_y, m_f = self.weights.shape
+        m_size = m_x * m_y * m_f
+        outputs = [
+            self.activation.activate(
+                np.dot(
+                    self.weights[i_x, i_y, i_f, :, :, :].reshape((m_size,)),
+                    inputs[i_x * stride_x:i_x * stride_x + kernel_x,
+                    i_y * stride_y:i_y * stride_y + kernel_y,
+                    :].reshape((m_size,))
+                ) +
+                self.biases[i_x, i_y, i_f])
+            for i_x in range(n_x)
+            for i_y in range(n_y)
+            for i_f in range(n_f)]
+        self.outputs = outputs = np.array(outputs).reshape(self.shape)
+        return self.next_layer.feed_forward(outputs) if self.next_layer else outputs
+
+    def back_propagate(self, output_deltas):
+        input_deltas = self.activation.derivate(self.outputs, output_deltas)
+        stride_x, stride_y = self.strides
+        kernel_x, kernel_y = self.kernel_size
+        n_x, n_y, n_f, m_x, m_y, m_f = self.weights.shape
+        n_size = n_x * n_y * n_f
+        for i_x, i_y, i_f in [(x, y, f) for x in range(n_x) for y in range(n_y) for f in range(n_f)]:
+            input_delta = input_deltas[i_x, i_y, i_f]
+            self.biases[i_x, i_y, i_f] -= input_delta
+            self.weights[i_x, i_y, i_f] -= input_delta * self.inputs[i_x * stride_x:i_x * stride_x + kernel_x,
+                                                         i_y * stride_y:i_y * stride_y + kernel_y, :]
+        propagated_deltas = [np.dot(input_deltas.reshape((n_size,)),
+                                    self.weights[:, :, :, i_x, i_y, i_f].reshape((n_size,)))
+                             for i_x in range(m_x)
+                             for i_y in range(m_y)
+                             for i_f in range(m_f)]
+        propagated_deltas = np.array(propagated_deltas)
+        return self.prev_layer.back_propagate(propagated_deltas) if self.prev_layer else propagated_deltas
 
 
 class Activation(object):
@@ -122,7 +216,7 @@ class Activation(object):
     def activate(self, input):
         return input
 
-    def derivate(self, x, delta_x):
+    def derivatie(self, x, delta_x):
         return 1.
 
 
@@ -134,4 +228,4 @@ class SigmoidActivation(Activation):
         return 1. / (1. + np.exp(-input))
 
     def derivate(self, x, delta_x):
-        return x * (1. - x) * delta_x
+        return x * (1 - x) * delta_x
--- test/gtlearning_test/neuralnetwork_test.py
+++ test/gtlearning_test/neuralnetwork_test.py
@@ -1,79 +1,121 @@
 import unittest, numpy as np
 from gtlearning.neuralnetwork import *
 
-inputs = [np.array(l)
-          for l in [
-              [1, 1, 1, 1, 1,
-               1, 0, 0, 0, 1,
-               1, 0, 0, 0, 1,
-               1, 0, 0, 0, 1,
-               1, 1, 1, 1, 1],
-              [0, 0, 1, 0, 0,
-               0, 0, 1, 0, 0,
-               0, 0, 1, 0, 0,
-               0, 0, 1, 0, 0,
-               0, 0, 1, 1, 0],
-              [1, 1, 1, 1, 1,
-               0, 0, 0, 0, 1,
-               1, 1, 1, 1, 1,
-               1, 0, 0, 0, 0,
-               1, 1, 1, 1, 1],
-              [1, 1, 1, 1, 1,
-               0, 0, 0, 0, 1,
-               1, 1, 1, 1, 1,
-               0, 0, 0, 0, 1,
-               1, 1, 1, 1, 1],
-              [1, 0, 0, 0, 1,
-               1, 0, 0, 0, 1,
-               1, 1, 1, 1, 1,
-               0, 0, 0, 0, 1,
-               0, 0, 0, 0, 1],
-              [1, 1, 1, 1, 1,
-               1, 0, 0, 0, 0,
-               1, 1, 1, 1, 1,
-               0, 0, 0, 0, 1,
-               1, 1, 1, 1, 1],
-              [1, 1, 1, 1, 1,
-               1, 0, 0, 0, 0,
-               1, 1, 1, 1, 1,
-               1, 0, 0, 0, 1,
-               1, 1, 1, 1, 1],
-              [1, 1, 1, 1, 1,
-               0, 0, 0, 0, 1,
-               0, 0, 0, 0, 1,
-               0, 0, 0, 0, 1,
-               0, 0, 0, 0, 1],
-              [1, 1, 1, 1, 1,
-               1, 0, 0, 0, 1,
-               1, 1, 1, 1, 1,
-               1, 0, 0, 0, 1,
-               1, 1, 1, 1, 1],
-              [1, 1, 1, 1, 1,
-               1, 0, 0, 0, 1,
-               1, 1, 1, 1, 1,
-               0, 0, 0, 0, 1,
-               1, 1, 1, 1, 1],
-          ]]
-
-targets = [np.array([1 if i == j else 0 for i in range(10)])
-           for j in range(10)]
-
 
 class SequentialNetworkTestCase(unittest.TestCase):
+    def setUp(self):
+        small_inputs = [
+            [1, 1, 1,
+             1, 0, 1,
+             1, 0, 1,
+             1, 0, 1,
+             1, 1, 1],
+            [0, 1, 0,
+             0, 1, 0,
+             0, 1, 0,
+             0, 1, 0,
+             0, 1, 0],
+            [1, 1, 1,
+             0, 0, 1,
+             1, 1, 1,
+             1, 0, 0,
+             1, 1, 1],
+            [1, 1, 1,
+             0, 0, 1,
+             1, 1, 1,
+             0, 0, 1,
+             1, 1, 1],
+            [1, 0, 1,
+             1, 0, 1,
+             1, 1, 1,
+             0, 0, 1,
+             0, 0, 1],
+            [1, 1, 1,
+             1, 0, 0,
+             1, 1, 1,
+             0, 0, 1,
+             1, 1, 1],
+            [1, 1, 1,
+             1, 0, 0,
+             1, 1, 1,
+             1, 0, 1,
+             1, 1, 1],
+            [1, 1, 1,
+             0, 0, 1,
+             0, 0, 1,
+             0, 0, 1,
+             0, 0, 1],
+            [1, 1, 1,
+             1, 0, 1,
+             1, 1, 1,
+             1, 0, 1,
+             1, 1, 1],
+            [1, 1, 1,
+             1, 0, 1,
+             1, 1, 1,
+             0, 0, 1,
+             1, 1, 1],
+        ]
+        small_shape = (5, 3)
+
+        mid_inputs = [[small[i]
+                       for i in [0, 1, 1, 1, 2,
+                                 3, 4, 4, 4, 5,
+                                 3, 4, 4, 4, 5,
+                                 6, 7, 7, 7, 8,
+                                 9, 10, 10, 10, 11,
+                                 9, 10, 10, 10, 11,
+                                 12, 13, 13, 13, 14]]
+                      for small in small_inputs]
+        mid_shape = (7, 5)
+
+        big_inputs = [[small[i]
+                       for i in [0, 1, 1, 1, 1, 1, 2,
+                                 3, 4, 4, 4, 4, 4, 5,
+                                 3, 4, 4, 4, 4, 4, 5,
+                                 3, 4, 4, 4, 4, 4, 5,
+                                 6, 7, 7, 7, 7, 7, 8,
+                                 9, 10, 10, 10, 10, 10, 11,
+                                 9, 10, 10, 10, 10, 10, 11,
+                                 9, 10, 10, 10, 10, 10, 11,
+                                 12, 13, 13, 13, 13, 13, 14]]
+                      for small in small_inputs]
+        big_shape = (9, 7)
+        self.output_size = output_size = 10
+        inputs = [np.array(l)
+                  for l in mid_inputs][:output_size]
+        shape = mid_shape
+        self.inputs = inputs
+        self.targets = [np.array([1 if i == j else 0
+                                  for i in range(output_size)])
+                        for j in range(output_size)]
+        self.inputs2D = [np.reshape(arr, shape) for arr in inputs]
+
     def test_denselayers(self):
+        output_size = self.output_size
         network = SequentialNetwork().add(
-            DenseLayer(9, input_shape=(25,))).add(
-            DenseLayer(6)).add(
-            DenseLayer(10)).compile().fit(
-            inputs, targets, epochs=50000)
-        x_index = 7
-        y_pred = network.predict(inputs[x_index])
-        for y in y_pred:
-            print(y)
+            DenseLayer(12, input_shape=self.inputs[0].shape)).add(
+            DenseLayer(output_size)).compile().fit(
+            self.inputs, self.targets, epochs=4999, verbose=0)
+        for i in range(output_size):
+            print((network.predict(self.inputs[i]) * 100).tolist())
         self.assertTrue(True)
+
+    def test_conv2dlayers(self):
+        output_size = self.output_size
+        network = SequentialNetwork().add(
+            Conv2DLayer(2, kernel_size=(3,3), input_shape=self.inputs2D[0].shape)).add(
+            FlattenLayer()).add(
+            DenseLayer(output_size)).compile().fit(
+            self.inputs2D, self.targets, epochs=2999, verbose=0)
+        for i in range(output_size):
+            print((network.predict(self.inputs2D[i]) * 100).tolist())
+        self.assertTrue(True)
+
 
     def test_flattenLayers(self):
         pass  # todo: test when Conv2DLayer is ready
+
 
 
 if __name__ == '__main__':
